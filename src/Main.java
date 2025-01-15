6b978ef258343bc590caa289c8bc5b8205fe2305
Vagner Cezar <vagnercezar@gmail.com>
2025-01-15 01:48:00 -0300

    Creating instances on screen on main

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,14 @@ public class Main {
     public static void main(String[] args) {
 
 
+        Tela Tela1 = new Tela(1,"Tela P", "Quadrado",1.5);
+        Processador Processador1 = new Processador(2,"3.7","Nano", 5.0);
+        Memoria Memoria1 = new Memoria(3,"DDR3", "Slim", 4);
+        Jogo Jogo1 = new Jogo(4,"Mario","Aventura",  25);
+
+
+        Celular celular1 = new Celular(1,"3310", "Nokia", Tela1, Processador1, Memoria1);
+
+
     }
 }
